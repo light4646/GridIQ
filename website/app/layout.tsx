aa65c60166039f57bfd0c85054ac389e41ad68f0
Nour Zaki <nourmichael2006@gmail.com>
2026-05-25 20:30:05 -0400

    Point race analytics nav to events hub

--- website/app/layout.tsx
+++ website/app/layout.tsx
@@ -86,7 +86,7 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
             <Link href="/constructors">Constructors</Link>
             <Link href="/compare">Compare</Link>
             <Link href="/explore">Explorer</Link>
-            <Link href="/explore">Race Analytics</Link>
+            <Link href="/events">Race Analytics</Link>
           </nav>
         </header>
         {children}
